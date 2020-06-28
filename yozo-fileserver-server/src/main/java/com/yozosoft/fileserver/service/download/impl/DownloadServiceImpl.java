de008c44384c3ca04ef2367933cbf10d6b04b5bb
zhouf <513829633@qq.com>
2020-06-28 10:22:40 +0800

    1:fix 下载带.文件名不正确bug

--- yozo-fileserver-server/src/main/java/com/yozosoft/fileserver/service/download/impl/DownloadServiceImpl.java
+++ yozo-fileserver-server/src/main/java/com/yozosoft/fileserver/service/download/impl/DownloadServiceImpl.java
@@ -86,7 +86,7 @@ public class DownloadServiceImpl implements IDownloadService {
         if (StringUtils.isBlank(fileName)) {
             fileName = StorageConstant.MULTIPLE_DOWNLOAD_FILENAME;
         }
-        File zipFile = new File(zipDir, FilenameUtils.getBaseName(fileName) + ".zip");
+        File zipFile = new File(zipDir, fileName + ".zip");
         if (zipFile.exists()) {
             zipFile.delete();
         }
